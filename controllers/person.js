5fdaa893efd0730b5b36c619b3b119c888cd4bef
Michael <michael@avdvco.com>
2015-08-13 16:28:57 -0600

    added ngResource, ngRoute

---
+++ controllers/person.js
@@ -0,0 +1,8 @@
+// controller for person object
+var personController = {
+	index: function(req, res) {
+		res.send('person');
+	}
+};
+
+module.exports = personController;
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,23 @@
-angular.module('list', [])
-.controller('listController', ['$scope', function($scope){
+var List = angular.module('list', ["ngResource", "ngRoute"])
+
+List.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+				templateUrl	: '/template/home',
+				controller  : 'listController'
+		})
+		.when('', {
+				templateUrl	: '/template/persons',
+				controller  : 'personsController'
+		})
+})
+List.controller('listController', ['$scope', function($scope){
 	$scope.listItems = [
 		{
 			'itemName' : 'Blueberries',
 			//'itemName' : 'View UI',
 			'claimed' : false
 		}
-
 	]
 
 	$scope.addItem = function(){
@@ -17,19 +28,31 @@ angular.module('list', [])
 		//console.log('!', $scope.newListItem)
 		$scope.newListItem = ''
 	}
+
 	$scope.clearClaimed = function(){
 		$scope.listItems = $scope.listItems.filter(function(item){
 			if(!item.claimed){
 				return item
 			}
+
+
+
 		})
-		console.log('fileredItems ', $scope.listItems)
+		// console.log('fileredItems ', $scope.listItems)
 		// console.log('fileredItems ', $scope.clearClaimed)
 	}
 
 
 }]);
+
+//.personController
+// see data model whiteboard photo 
+// set up dynamic routing
+// persist user list to db
+// create person object to array of items for that person (each item can be associated to a store)
 // user logs in, oAuth for fbConnect, gmail, etc.
-// screen needs to show their items (dropdown to see only items by store- at currently)
-// and present a list of the people who have items requested at that store
+// after login, screen needs to show 2 lists:
+// 1. their items (dropdown to see only items by store- at currently)
+// 2. and present a list of the people who have items requested at that store
 /// from the list of people, select to show their list of items	
+// function to create shopping list based on route for store optimized by shortest.
